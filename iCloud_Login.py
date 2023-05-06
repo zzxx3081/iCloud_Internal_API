576784de68c50aa67b992d3f5883526471e728d4
zzxx3081 <zzxx3081@naver.com>
2023-05-07 00:08:20 +0900

    Third Commit Login Password Masking

--- iCloud_Login.py
+++ iCloud_Login.py
@@ -1,12 +1,18 @@
-import os, requests, json, urllib3
+import os
+import sys
+import requests
+import json
+import urllib3
+import msvcrt
+from getpass import getpass
 from iCloud_Session import Session
 from termcolor import colored
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {
-   'http': 'http://127.0.0.1:8888',
-   'https': 'http://127.0.0.1:8888'
+    'http': 'http://127.0.0.1:8888',
+    'https': 'http://127.0.0.1:8888'
 }
 
 
@@ -17,11 +23,9 @@ def Authentication_NewToken() -> dict:
     print("Moreover, If you know \"TrustToken\" of the iCloud Account, You'll be able to bypass the Two-Factor-Authentication.")
     print("The iCloud Login process requires Two-Factor-Authentication by default.")
     print("In order to success the Two-Factor-Authentication, You should input the 6-digit Security Code which can get from Apple Device connected by the iCloud Account.\n")
-    
-    print(colored("Submit your iCloud Account Information.", 'green'))
-    iCloud_ID = input(colored("iCloud ID: ", 'yellow'))
-    iCloud_PW = input(colored("iCloud PW: ", 'yellow'))
 
+    print(colored("Submit your iCloud Account Information.", 'green'))
+    iCloud_ID, iCloud_PW = input_iCloud_Credential()
 
     while True:
         print(colored("\nDo you know the Account's TrustToken? (Y/N)", 'yellow'))
@@ -29,16 +33,17 @@ def Authentication_NewToken() -> dict:
 
         if Answer.lower() == 'y':
             print(colored("\nInput the Account's TrustToken.", 'yellow'))
-            print(colored("[Reference] If you submit the Invalid TrustToken, you'll move to Two-Factor-Authentication process Automatically.", 'green'))
+            print(colored(
+                "[Reference] If you submit the Invalid TrustToken, you'll move to Two-Factor-Authentication process Automatically.", 'green'))
             TrustToken = input(colored("TrustToken: ", 'yellow'))
             break
         elif Answer.lower() == 'n':
-            print(colored("\n[Default] You need to perform the Two-Factor-Authentication\n", 'yellow'))
+            print(colored(
+                "\n[Default] You need to perform the Two-Factor-Authentication\n", 'yellow'))
             break
         else:
             print(colored("\n[Invalid Input] Try Again!\n", 'yellow'))
 
-    
     # Login Based Session Authentication Start!
     iCloud_Login_Class = iCloud_Auth_Session(iCloud_ID, iCloud_PW)
 
@@ -50,17 +55,20 @@ def Authentication_NewToken() -> dict:
 
         if Answer.lower() == 'y':
             print(colored("\nSubmit the Output Path to save the Session Data.", 'green'))
-            iCloud_Session_Path = dequote(input(colored("iCloud Session Path: ", 'yellow')))
+            iCloud_Session_Path = dequote(
+                input(colored("iCloud Session Path: ", 'yellow')))
 
             while not os.path.exists(iCloud_Session_Path):
                 print(colored("\n[Invalid Path] Try Again!\n", 'yellow'))
-                iCloud_Session_Path = input(colored("iCloud Session Path: ", 'yellow'))
+                iCloud_Session_Path = input(
+                    colored("iCloud Session Path: ", 'yellow'))
 
             iCloud_Login_Class.saveSession(iCloud_Session_Path)
             break
-        
+
         elif Answer.lower() == 'n':
-            print(colored("\n[warning]If the program shut down, You can't use the iCloud Session Data again.", 'red'))
+            print(colored(
+                "\n[warning]If the program shut down, You can't use the iCloud Session Data again.", 'red'))
             break
         else:
             print(colored("\n[Invalid Input] Try Again!\n", 'yellow'))
@@ -68,30 +76,73 @@ def Authentication_NewToken() -> dict:
     return iCloud_Login_Class.getSesssion()
 
 
-
 # Get a old iCloud Session Data through Local Session File
 def Authentication_FileToken() -> dict:
 
-    print("To acquire a old Session Informaion from Local Session File, You need to input the Session File Path.\n") 
+    print("To acquire a old Session Informaion from Local Session File, You need to input the Session File Path.\n")
     print(colored("Submit the Full Path about Local Session File (Format : Json).", 'green'))
-    Local_Session_Path = dequote(input(colored("Local Session File Path: ", 'yellow')))
+    Local_Session_Path = dequote(
+        input(colored("Local Session File Path: ", 'yellow')))
 
     while not os.path.exists(Local_Session_Path):
         print(colored("\n[Invalid Path] Try Again!\n", 'yellow'))
-        Local_Session_Path = input(colored("Local Session File Path: ", 'yellow'))
+        Local_Session_Path = input(
+            colored("Local Session File Path: ", 'yellow'))
 
     iCloud_Session_Class = Session()
 
     return iCloud_Session_Class.readSession(Local_Session_Path)
 
 
+# Remove input "\'" or "\""  (ex: 'path' -> path)
+def input_iCloud_Credential():
+    iCloud_ID = input(colored("iCloud ID: ", 'yellow'))
+    iCloud_PW = ""
+    print(colored("iCloud PW: ", 'yellow'), end='', flush=True)
+    len_Of_PW = 0
+
+    while True:
+        pwChar = msvcrt.getch()
+
+        # Case of newline
+        if pwChar == b'\r':
+            print()
+            print(iCloud_PW)
+            break
+
+        # Case of backspace
+        elif pwChar == b'\b':
+            if len_Of_PW < 1:
+                pass
+            else:
+                msvcrt.putch(b'\b')
+                msvcrt.putch(b' ')
+                msvcrt.putch(b'\b')
+                pwChar = pwChar[:-1]
+                len_Of_PW -= 1
+
+        # [Arrow] Key Exception
+        elif pwChar == b'\xe0':
+            msvcrt.getch()
+
+        # [Tab] Key Exception
+        elif pwChar in [b'\t']:
+            continue
+
+        # [Ctrl + z] or [Ctrl + c] -> Shut Down
+        elif pwChar in [b'\x03', b'\x1a']:
+            print(colored("\n[Shut Down]", 'yellow'))
+            sys.exit()
+
+        else:
+            print("●", end='', flush=True)
+            iCloud_PW += pwChar.decode("utf-8")
+            len_Of_PW += 1
+
+    return iCloud_ID, iCloud_PW
+
 
 def dequote(s):
-    """
-    If a string has single or double quotes around it, remove them.
-    Make sure the pair of quotes match.
-    If a matching pair of quotes is not found, return the string unchanged.
-    """
     if (s[0] == s[-1]) and s.startswith(("'", '"')):
         return s[1:-1]
     return s
@@ -101,14 +152,14 @@ def dequote(s):
 class iCloud_Auth_Session(Session):
 
     # Require iCloud ID, PW and TrustToken (Default Empty String)
-    def __init__(self, ID, Password, TrustToken = ''):
+    def __init__(self, ID, Password, TrustToken=''):
 
-        super().__init__() # iCloud_Session Class Init
+        super().__init__()  # iCloud_Session Class Init
 
         # iCloud Account Information
-        self.SessionJson['AccountInfo'] = { 
-            'iCloud ID' : ID, 
-            'iCloud PW' : Password 
+        self.SessionJson['AccountInfo'] = {
+            'iCloud ID': ID,
+            'iCloud PW': Password
         }
 
         # iCloud Headers
@@ -118,11 +169,11 @@ class iCloud_Auth_Session(Session):
         self.SessionJson['AccountSessions'] = {}
 
         # Start the First Auth
-        self.First_Signin_Request(TrustToken) 
-
+        self.First_Signin_Request(TrustToken)
 
     # First Auth Signin Request
-    def First_Signin_Request(self, TrustToken = ''):
+
+    def First_Signin_Request(self, TrustToken=''):
         try:
             requestURL = "https://idmsa.apple.com/appleauth/auth/signin"
 
@@ -133,18 +184,19 @@ class iCloud_Auth_Session(Session):
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.1 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.1',
                 'Origin': 'https://idmsa.apple.com',
                 'X-Requested-With': 'XMLHttpRequest',
-                'X-Apple-Widget-Key' : 'd39ba9916b7251055b22c7f910e2ea796ee65e98b2ddecea8f5dde8d9d1a815d'
+                'X-Apple-Widget-Key': 'd39ba9916b7251055b22c7f910e2ea796ee65e98b2ddecea8f5dde8d9d1a815d'
             }
 
             data = {
-                'accountName' : self.SessionJson['AccountInfo']['iCloud ID'],
-                'password' : self.SessionJson['AccountInfo']['iCloud PW'],
-                'rememberMe' : True,
-                'trustTokens' : [TrustToken]
+                'accountName': self.SessionJson['AccountInfo']['iCloud ID'],
+                'password': self.SessionJson['AccountInfo']['iCloud PW'],
+                'rememberMe': True,
+                'trustTokens': [TrustToken]
             }
 
             postData = json.dumps(data)
-            response = requests.post(url=requestURL, headers=header, data=postData, proxies=proxies, verify=False)
+            response = requests.post(
+                url=requestURL, headers=header, data=postData, proxies=proxies, verify=False)
 
             # Get First Auth Token
             self.SessionJson['AccountHeaders']['scnt'] = response.headers['scnt']
@@ -153,45 +205,46 @@ class iCloud_Auth_Session(Session):
             self.SessionJson['AccountHeaders']['X-Apple-Session-Token'] = response.headers['X-Apple-Session-Token']
             self.SessionJson['AccountHeaders']['X-Apple-ID-Account-Country'] = response.headers['X-Apple-ID-Account-Country']
             self.SessionJson['AccountHeaders']['X-Apple-ID-Session-Id'] = response.headers['X-Apple-ID-Session-Id']
-            
 
             # Check whether to Second Auth (If the TrustToken is empty or invalid than need to perform the Two-Factor-Authentication process)
-            if response.headers.get('X-Apple-TwoSV-Trust-Eligible') != None: # If the Two-Factor-Authentication process is required, this value is setted true
-                self.Second_Securitycode_Request() # Update the X-Apple-Session-Token
-                self.Get_TrustToken_Request() # Get TrustToken
-                self.AccountLogin_Request() # Get WEB AUTH, PCS cookies
+            # If the Two-Factor-Authentication process is required, this value is setted true
+            if response.headers.get('X-Apple-TwoSV-Trust-Eligible') != None:
+                self.Second_Securitycode_Request()  # Update the X-Apple-Session-Token
+                self.Get_TrustToken_Request()  # Get TrustToken
+                self.AccountLogin_Request()  # Get WEB AUTH, PCS cookies
 
             else:
                 self.SessionJson['AccountHeaders']['X-Apple-TwoSV-Trust-Token'] = TrustToken
-                self.AccountLogin_Request() # Get WEB AUTH, PCS cookies
-        
+                self.AccountLogin_Request()  # Get WEB AUTH, PCS cookies
+
         except requests.exceptions.RequestException as e:
             print("[Fail] First Auth Signin Request", e)
-        
 
     # Second Auth Securitycode Request
+
     def Second_Securitycode_Request(self):
         try:
             requestURL = "https://idmsa.apple.com/appleauth/auth/verify/trusteddevice/securitycode"
 
             header = {
-                'Content-Type' : 'application/json',
-                'Referer' : 'https://idmsa.apple.com/',
-                'scnt' : self.SessionJson['AccountHeaders']['scnt'],
-                'X-Apple-ID-Session-Id' : self.SessionJson['AccountHeaders']['X-Apple-ID-Session-Id'],
-                'X-Apple-Widget-Key' : self.SessionJson['AccountHeaders']['X-Apple-Widget-Key'],
+                'Content-Type': 'application/json',
+                'Referer': 'https://idmsa.apple.com/',
+                'scnt': self.SessionJson['AccountHeaders']['scnt'],
+                'X-Apple-ID-Session-Id': self.SessionJson['AccountHeaders']['X-Apple-ID-Session-Id'],
+                'X-Apple-Widget-Key': self.SessionJson['AccountHeaders']['X-Apple-Widget-Key'],
             }
 
             data = {
-                'securityCode' : {
-                    'code' : input(colored("Security Code(6-digit): ", 'yellow'))
+                'securityCode': {
+                    'code': input(colored("Security Code(6-digit): ", 'yellow'))
                 }
             }
 
             postData = json.dumps(data)
 
             # If allow the redirect, can't get updated X-Apple-Session-Token
-            response = requests.post(url=requestURL, headers=header, data=postData, proxies=proxies, verify=False, allow_redirects=False)
+            response = requests.post(url=requestURL, headers=header, data=postData,
+                                     proxies=proxies, verify=False, allow_redirects=False)
 
             # After the Second Auth, update the X-Apple-Session-Token
             self.SessionJson['AccountHeaders']['X-Apple-Session-Token'] = response.headers['X-Apple-Session-Token']
@@ -199,22 +252,22 @@ class iCloud_Auth_Session(Session):
         except requests.exceptions.RequestException as e:
             print("[Fail] Second Auth Securitycode Reqeust", e)
 
+    # Get TrustToken
 
-    # Get TrustToken 
     def Get_TrustToken_Request(self):
         try:
             requestURL = "https://idmsa.apple.com/appleauth/auth/2sv/trust"
 
             header = {
-                'Content-Type' : 'application/json',
-                'Referer' : 'https://idmsa.apple.com/',
-                'scnt' : self.SessionJson['AccountHeaders']['scnt'],
-                'X-Apple-ID-Session-Id' : self.SessionJson['AccountHeaders']['X-Apple-ID-Session-Id'],
-                'X-Apple-Widget-Key' : self.SessionJson['AccountHeaders']['X-Apple-Widget-Key'],
+                'Content-Type': 'application/json',
+                'Referer': 'https://idmsa.apple.com/',
+                'scnt': self.SessionJson['AccountHeaders']['scnt'],
+                'X-Apple-ID-Session-Id': self.SessionJson['AccountHeaders']['X-Apple-ID-Session-Id'],
+                'X-Apple-Widget-Key': self.SessionJson['AccountHeaders']['X-Apple-Widget-Key'],
             }
 
-            response = requests.get(url=requestURL, headers=header, proxies=proxies, verify=False)
-
+            response = requests.get(
+                url=requestURL, headers=header, proxies=proxies, verify=False)
 
             # Aquire the TrusToken and updated X-Apple-Session-Token
             self.SessionJson['AccountHeaders']['X-Apple-Session-Token'] = response.headers['X-Apple-Session-Token']
@@ -223,8 +276,8 @@ class iCloud_Auth_Session(Session):
         except requests.exceptions.RequestException as e:
             print("[Fail] TrustToken Reqeust", e)
 
-
     # Get the Auth cookis about PCS and WEB AUTH (AccountLogin Request)
+
     def AccountLogin_Request(self):
 
         try:
@@ -232,22 +285,22 @@ class iCloud_Auth_Session(Session):
 
             header = {
                 'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'Accept-Language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.1 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.1',
-                'Referer' : 'https://www.icloud.com/',
+                'Referer': 'https://www.icloud.com/',
                 'Origin': 'https://www.icloud.com',
                 'Connection': 'keep-alive',
             }
 
             data = {
-                'dsWebAuthToken' : self.SessionJson['AccountHeaders']['X-Apple-Session-Token'],
-                'accountCountryCode' : self.SessionJson['AccountHeaders']['X-Apple-ID-Account-Country'],
-                'extended_login' : "true"
+                'dsWebAuthToken': self.SessionJson['AccountHeaders']['X-Apple-Session-Token'],
+                'accountCountryCode': self.SessionJson['AccountHeaders']['X-Apple-ID-Account-Country'],
+                'extended_login': "true"
             }
 
             postData = json.dumps(data)
-            response = requests.post(url=requestURL, headers=header, data=postData, proxies=proxies, verify=False)
-            
+            response = requests.post(
+                url=requestURL, headers=header, data=postData, proxies=proxies, verify=False)
 
             # After the Second Auth, get the PCS, WEB-AUTH cookies
             for cookie in response.cookies:
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import os, sys
+import os
+import sys
 import iCloud_Login
 from pyfiglet import Figlet
 from termcolor import colored
 
 # iCloud breaker Main Intro
+
+
 def Main_intro():
     os.system('cls')
     f = Figlet(font='big')
@@ -16,17 +19,20 @@ def Main_intro():
 
 # Get iCloud Session Data
 def Get_iCloud_Authentication_Session():
-    
+
     while True:
         print(colored("\n[iCloud Authentication Menu]", 'yellow'))
         print("Select the Method of iCloud Authentication! There are Two Authentication methods.")
         print("If you have Session Information File (Format: Json) on your Local Storage, you should select \"File Based Session Authentication\" (Number 1). ")
         print("Otherwise, you have to get a new Session Informaion through iCloud Account Management Server.")
         print("If you want to make the new Session, you can choose \"Login Based Session Authentication\" (Number 2).")
-        print(colored("[warning] Don't select the option that \"Login Based Session Authentication\" (Number 2) too often.", 'red'))
-        print(colored("[warning] Since \"iCloud Breaker\" use Internal API based Authentication Method, iCloud Account Management Server may detect it.", 'red'))
-        print(colored("[warning] That is, Your IP or Apple account can be blocked by Apple.\n\n", 'red'))
-        
+        print(colored(
+            "[warning] Don't select the option that \"Login Based Session Authentication\" (Number 2) too often.", 'red'))
+        print(colored(
+            "[warning] Since \"iCloud Breaker\" use Internal API based Authentication Method, iCloud Account Management Server may detect it.", 'red'))
+        print(colored(
+            "[warning] That is, Your IP or Apple account can be blocked by Apple.\n\n", 'red'))
+
         print("#    0. EXIT                                     #")
         print("#    1. File Based Session Authentication        #")
         print("#    2. Login Based Session Authentication       #")
@@ -41,27 +47,27 @@ def Get_iCloud_Authentication_Session():
         elif Number == 1:
             print(colored("\n[File Based Session Authentication]", 'yellow'))
             return iCloud_Login.Authentication_FileToken()
-        
+
         elif Number == 2:
             print(colored("\n[Login Based Session Authentication]", 'yellow'))
             return iCloud_Login.Authentication_NewToken()
-        
+
         elif Number == 3:
             continue
 
         else:
             print("[Invalid Number] Try Again!")
-    
+
+
 # main
 if __name__ == "__main__":
-    
+
     # Intro
     Main_intro()
-    
+
     # Get User Account Session
     Account_Session = Get_iCloud_Authentication_Session()
 
     # iCloud Drive
 
     # iCloud Mail
-    
